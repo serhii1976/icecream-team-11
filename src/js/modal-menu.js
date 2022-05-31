31fa86d286181dd905aa36c2701e6a19f9b7524e
DenisVoron <Denis.voronko@gmail.com>
2022-05-31 18:02:03 +0300

    add modal menu

--- src/js/modal-menu.js
+++ src/js/modal-menu.js
@@ -1,3 +1,19 @@
+//modal menu header
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
